53b8125a3744773bad2531410b7ff8e7613164b6
shubhangpatel <shubhangpatel06@gmail.com>
2024-03-24 09:35:03 +0530

    first commit

---
+++ api/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, File,UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow requests from all origins
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+MODEL=tf.keras.models.load_model("../models/1")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+@app.get("/ping")
+async def ping():
+     return "hello i am alive"
+
+def read_file_as_image(data)-> np.ndarray:
+     image=np.array(Image.open(BytesIO(data)))
+     return image
+
+@app.post("/predict")
+async def predict(
+    file :UploadFile= File(...)  
+):
+    image=read_file_as_image(await file.read())
+    img_batch=np.expand_dims(image,0)
+    predictions= MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    print(confidence)
+    print(predicted_class)
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__== "__main__":
+     uvicorn.run(app,host='localhost',port=8000)
